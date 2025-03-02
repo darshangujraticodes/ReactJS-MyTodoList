f9a912874dc368c373fa3ccea8320ed7352880ca
darshangujraticodes <darshangujrati5@gmail.com>
2025-03-02 11:10:50 +0530

    todo

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,17 @@ function App() {
   return (
     <>
       <div>
-        <div>
-          <p className="mt-3">
-            <a className="coder" href="https://darshan-gujrati.netlify.app/">
-              Crafted By Darshan Gujrati
-            </a>
-          </p>
+        <div className="mt-3">
+          <a className="coder" href="https://darshan-gujrati.netlify.app/">
+            Crafted By Darshan Gujrati
+          </a>
+          |
+          <a
+            className="links ms-3"
+            href="https://github.com/darshangujraticodes/ReactJS-MyTodoList"
+          >
+            Github Repo.
+          </a>
         </div>
         <TaskForm />
         <Task />
